42ab95f9b9ffa4947946ade1276efbcccb843667
Jhos12231 <jhosmerandersonquispetorres@gmail.com>
2024-07-24 17:20:42 -0500

    video

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Video from "./componet/video";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aplicacion web de video</h1>
+      <Video/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componet/video.js
@@ -0,0 +1,9 @@
+import "./video.css";
+
+function Video(){
+    return (<video controls>
+        <source src="https://server-video-public1.s3.us-east-2.amazonaws.com/bloodborne+edit++carol+of+the+Bells..mp4" type="video/mp4" />
+    </video>);
+
+}
+export default Video;
